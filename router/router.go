57859e43756d681b513ad326e140e65913575059
dreambo8563 <dreambo8563@outlook.com>
2018-04-08 14:57:48 +0800

    rm(swagger): rm swagger feature
    * plan to change to apidoc for easy usage

--- router/router.go
+++ router/router.go
@@ -2,24 +2,24 @@ package router
 
 import (
 	"path"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
-	// import the generated doc package
-	_ "vincent.com/golangginrest/docs"
 )
 
 // Init - router collection and controller dispatch
 func Init() {
 	// default config include the recover and logger middleware
 	router := gin.Default()
+	// swaggerGroup(router)
 	v1Prefix := "/api/v1"
-	swaggerGroup(router)
 	todoGroup(router, v1Prefix)
 	authGroup(router, v1Prefix)
 	// SPA fallback router
 	router.NoRoute(func(c *gin.Context) {
 		dir, file := path.Split(c.Request.RequestURI)
-		if file == "" {
+		ext := filepath.Ext(file)
+		if file == "" || ext == "" {
 			c.File("./public/index.html")
 		} else {
 			c.File("./public" + path.Join(dir, file))
